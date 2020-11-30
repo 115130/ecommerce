b368857a89194db6b05867c6f52ad5a54c49cff2
yangjiahui <wodeqiyuxin@gmial.com>
2020-11-30 22:50:06 +0800

    dao编写完成

--- src/g305/service/UserService.java
+++ src/g305/service/UserService.java
@@ -1,8 +1,28 @@
 package g305.service;
 
 import g305.dao.UserDao;
+import g305.pojo.User;
+
+import java.util.List;
 
 public class UserService {
     UserDao userDao = new UserDao();
+
+    /**
+     * 获取所有人的信息
+     * @return 所有人的信息
+     */
+    public List<User> getAllUser(){
+        return userDao.getAllUser();
+    }
+
+    /**
+     * 获取已删除的人的信息
+     * @return 已删除的人的信息
+     */
+    public List<User> getDeleteUser(){
+        return userDao.getDeleteUser();
+    }
+
 
 }
