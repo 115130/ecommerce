61c0c50ce84dca840e92a9ceab8866891c75054e
yangjiahui <wodeqiyuxin@gmial.com>
2020-12-01 21:37:55 +0800

    service完成俩

--- src/g305/service/ProductService.java
+++ src/g305/service/ProductService.java
@@ -1,7 +1,100 @@
 package g305.service;
 
 import g305.dao.ProductDao;
+import g305.pojo.Product;
+
+import java.io.InputStream;
+import java.sql.Blob;
+import java.util.List;
 
 public class ProductService {
     ProductDao productDao = new ProductDao();
+    /**
+     *查询所有的商品
+     * @return 全部商品
+     */
+    public List<Product> getAllProduct(){
+        return productDao.getAllProduct();
+    }
+
+    /**
+     *模糊查询和完整名字查询商品
+     * @param productName 商品名
+     * @return 指定名字商品
+     */
+    public List<Product> getNameProduct(String productName){
+        return productDao.getNameProduct(productName);
+    }
+
+    /**
+     *增加商品
+     * @param productName 商品名
+     * @param productCount 商品数量
+     * @param productPicture 商品照片
+     * @param productProperty 商品属性
+     * @return 增加的数量
+     */
+    public int addProduct(String productName, int productCount, double productPrice, InputStream productPicture, String productProperty){
+        return productDao.addProduct(productName,productCount,productPrice,productPicture,productProperty);
+    }
+
+    /**
+     *删除商品
+     * @param productId 商品id
+     * @return 删除的数量
+     */
+    public int deleteProduct(int productId){
+        return productDao.deleteProduct(productId);
+    }
+
+    /**
+     *据商品名改变商品数量
+     * @param productName 商品名
+     * @param afterCount 更改商品数量
+     * @return 更改的数量
+     */
+    public int updateCount(String productName,int afterCount){
+        return productDao.updateCount(productName,afterCount);
+    }
+
+    /**
+     *根据商品名改变商品价格
+     * @param productName 商品名
+     * @param afterPrice 更改商品价格
+     * @return 更改的数量
+     */
+    public int updatePrice(String productName,double afterPrice){
+        return productDao.updatePrice(productName,afterPrice);
+    }
+
+    /**
+     *根据商品名改变商品图片
+     * @param productName 商品名
+     * @param afterImage 更改商品照片
+     * @return 更改的数量
+     */
+    public int updateImage(String productName, InputStream afterImage){
+        return productDao.updateImage(productName,afterImage);
+    }
+
+    /**
+     *根据商品名改变商品属性
+     * @param productName 商品名
+     * @param afterProperty 更改商品属性
+     * @return 更改的数量
+     */
+    public int updateProperty(String productName,String afterProperty){
+        return productDao.updateProperty(productName,afterProperty);
+    }
+
+    /**
+     *根据商品名改变商品名字
+     * @param productName 商品名
+     * @param afterName 更改商品名字
+     * @return 更改的数量
+     */
+    public int updateName(String productName,String afterName){
+        return productDao.updateName(productName,afterName);
+    }
+
 }
--- src/g305/service/UserService.java
+++ src/g305/service/UserService.java
@@ -23,6 +23,99 @@ public class UserService {
     public List<User> getDeleteUser(){
         return userDao.getDeleteUser();
     }
+    /**
+     * 删除用户
+     * @return 删除用户的数量
+     */
+    public int DELETE_USER(){
+        return userDao.DELETE_USER();
+    }
 
+    /**
+     * 名字查询用户
+     * @param name 用户名
+     * @return 返回指定的用户
+     */
+    public List<User> getUser(String name){
+        return userDao.getUser(name);
+    }
 
+    /**
+     * 恢复指定删除的用户
+     * @param name 用户名
+     * @return 恢复的用户数量
+     */
+    public int recDeleteUser(String name){
+        return userDao.recDeleteUser(name);
+    }
+
+    /**
+     * 删除用户
+     * @param name 用户名
+     * @return 返回删除的数量
+     */
+    public int deleteUser(String name){
+        return userDao.deleteUser(name);
+    }
+
+    /**
+     * 改名字
+     * @param name 名字
+     * @param afterName 更改之后的名字
+     * @return 修改了多少名字
+     */
+    public int updateName(String name,String afterName){
+        return userDao.updateName(name,afterName);
+    }
+
+    /**
+     *
+     * @param name 名字
+     * @param afterPetName 更改之后的名字
+     * @return 更改了多少行
+     */
+    public int updatePetName(String name,String afterPetName){
+        return userDao.updatePetName(name,afterPetName);
+    }
+
+    /**
+     *
+     * @param name 名字
+     * @param afterPassword 更改之后的密码
+     * @return 更改了多少行
+     */
+    public int updatePassword(String name,String afterPassword){
+        return userDao.updatePassword(name,afterPassword);
+    }
+
+    /**
+     *
+     * @param name 名字
+     * @param afterAddress 更改之后的地址
+     * @return 更改了多少行
+     */
+    public int updateAddress(String name,String afterAddress){
+        return userDao.updateAddress(name,afterAddress);
+    }
+
+    /**
+     *
+     * @param name 名字
+     * @param afterBalance 更改之后的余额
+     * @return 更改了多少行
+     */
+    public int updateBalance(String name,double afterBalance){
+        return userDao.updateBalance(name,afterBalance);
+    }
+
+    /**
+     *
+     * @param name 名字
+     * @param userPetName 昵称
+     * @param userPassword 密码
+     * @return 添加了多少行
+     */
+    public int addUser(String name,String userPetName,String userPassword){
+        return userDao.addUser(name,userPetName,userPassword);
+    }
 }
