ee286e08d1776f01afe339f1400d3fd16e3d6d2e
yangjiahui <wodeqiyuxin@gmial.com>
2020-12-01 21:56:06 +0800

    service完成俩

--- src/g305/service/ShippingListService.java
+++ src/g305/service/ShippingListService.java
@@ -1,7 +1,83 @@
 package g305.service;
 
 import g305.dao.ShippingListDao;
+import g305.pojo.ShippingList;
+
+import java.sql.Date;
+import java.util.List;
 
 public class ShippingListService {
     ShippingListDao shippingListDao=new ShippingListDao();
+
+    /**
+     * 查询所有的订单
+     * @return 所有订单
+     */
+    public List<ShippingList> getAllShippingList(){
+        return shippingListDao.getAllShippingList();
+    }
+
+    /**
+     *按订单号查询订单
+     * @param oder 订单号
+     * @return 订单号指定订单
+     */
+    public ShippingList getOneShippingList(String oder){
+        return shippingListDao.getOneShippingList(oder);
+    }
+
+    /**
+     *使用商品名模糊或者完整的名字查询订单
+     * @param productName 商品名
+     * @return 名字商品名
+     */
+    public List<ShippingList> getNameShippingList(String productName){
+        return shippingListDao.getNameShippingList(productName);
+    }
+
+    /**
+     *添加订单
+     * @param productId 商品id
+     * @param userId 用户id
+     * @param oder 订单号
+     * @param Address 地址
+     * @param money 总额
+     * @param status 订单状态
+     * @param complete_date 完成日期
+     * @return 返回订单增加数量
+     */
+    public int addShippingList(int productId, int userId, String oder, String Address, double money, String status, Date complete_date){
+        return shippingListDao.addShippingList(productId, userId, oder, Address, money, status, complete_date);
+    }
+
+    /**
+     *删除订单
+     * @param oder 订单号
+     * @return 删除订单数量
+     */
+    public int deleteShippingList(String oder){
+        return shippingListDao.deleteShippingList(oder);
+    }
+
+    /**
+     *更改地址
+     * @param Address 地址
+     * @param status 订单状态
+     * @param oder 订单号
+     * @return 返回更改了多少地址
+     */
+    public int updateAddressShippingList(String Address,String status,String oder){
+        return shippingListDao.updateAddressShippingList(Address, status, oder);
+    }
+
+    /**
+     *更改完成状态
+     * @param status 订单状态
+     * @param oder 订单号
+     * @return 返回更改多少
+     */
+    public int updateOderShippingList(String status,String oder){
+        return shippingListDao.updateOderShippingList(status, oder);
+    }
+
 }
--- src/g305/service/ShippingService.java
+++ src/g305/service/ShippingService.java
@@ -1,7 +1,72 @@
 package g305.service;
 
 import g305.dao.ShippingDao;
+import g305.pojo.Shipping;
+
+import java.util.List;
 
 public class ShippingService {
     ShippingDao shippingDao = new ShippingDao();
+
+    /**
+     *获取全部的购物车
+     * @return 全部的购物车
+     */
+    public List<Shipping> getAllShipping(){
+        return shippingDao.getAllShipping();
+    }
+
+    /**
+     *使用用户id查全部
+     * @param id 用户id
+     * @return 用户id的购物车
+     */
+    public List<Shipping> getArsShipping(int id){
+        return shippingDao.getArsShipping(id);
+    }
+
+    /**
+     *更改商品属性
+     * @param userId 用户id
+     * @param productId 商品id
+     * @param afterProperty 商品属性
+     * @return 更改的数量
+     */
+    public int updateProperty(int userId,int productId,String afterProperty){
+        return shippingDao.updateProperty(userId,productId,afterProperty);
+    }
+
+    /**
+     *增加商品
+     * @param userId 用户id
+     * @param productId 商品id
+     * @param productCount 商品数量
+     * @param productProperty 商品属性
+     * @return 更改的数量
+     */
+    public int addShipping(int userId,int productId,int productCount,String productProperty){
+        return shippingDao.addShipping(userId,productId,productCount,productProperty);
+    }
+
+    /**
+     *更改商品属性
+     * @param userId 用户id
+     * @param productId 商品id
+     * @param afterCount 商品数量
+     * @return 更改的数量
+     */
+    public int updateProductCount(int userId,int productId,int afterCount){
+        return shippingDao.updateProductCount(userId,productId,afterCount);
+    }
+
+    /**
+     *删除购物车
+     * @param userId 用户id
+     * @param productId  商品id
+     * @return 返回删除的数量
+     */
+    public int deleteShipping(int userId,int productId){
+        return shippingDao.deleteShipping(userId,productId);
+    }
+
 }
